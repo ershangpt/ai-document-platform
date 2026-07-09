35069169506e9b0f56a0022c2e5f07d97e0fd699
gaurav-jkh <gaurav.vanani@vieraconsulting.com>
2026-07-09 15:17:08 +0530

    Day 4: Implement validation

--- backend/user-service/src/main/java/com/shan/aidoc/userservice/controller/UserController.java
+++ backend/user-service/src/main/java/com/shan/aidoc/userservice/controller/UserController.java
@@ -3,6 +3,7 @@ package com.shan.aidoc.userservice.controller;
 import com.shan.aidoc.userservice.dto.CreateUserRequest;
 import com.shan.aidoc.userservice.entity.User;
 import com.shan.aidoc.userservice.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody CreateUserRequest body) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody CreateUserRequest body) {
         User user = userService.createUser(body);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
--- backend/user-service/src/main/java/com/shan/aidoc/userservice/dto/CreateUserRequest.java
+++ backend/user-service/src/main/java/com/shan/aidoc/userservice/dto/CreateUserRequest.java
@@ -1,8 +1,14 @@
 package com.shan.aidoc.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateUserRequest (
+        @NotBlank(message = "First name is required")
         String firstName,
+        @NotBlank(message = "Last name is required")
         String lastName,
+        @Email(message = "Email must be valid")
         String email
 ){
 }
