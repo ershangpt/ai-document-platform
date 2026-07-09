3e520235e5107ad67039f7a23d65ffbf810eb8fd
gaurav-jkh <gaurav.vanani@vieraconsulting.com>
2026-07-09 20:37:45 +0530

    Day 4: Add global exception handling for validation errors

--- backend/user-service/src/main/java/com/shan/aidoc/userservice/controller/GlobalExceptionHandler.java
+++ backend/user-service/src/main/java/com/shan/aidoc/userservice/controller/GlobalExceptionHandler.java
@@ -1,4 +1,36 @@
 package com.shan.aidoc.userservice.controller;
 
+import com.shan.aidoc.userservice.exception.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleException (MethodArgumentNotValidException exception) {
+        List<String> errors = exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        ApiErrorResponse response = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Failed",
+                errors
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }
--- backend/user-service/src/main/java/com/shan/aidoc/userservice/exception/ApiErrorResponse.java
+++ backend/user-service/src/main/java/com/shan/aidoc/userservice/exception/ApiErrorResponse.java
@@ -1,4 +1,15 @@
 package com.shan.aidoc.userservice.exception;
 
-public class ApiErrorResponse {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ApiErrorResponse (
+        LocalDateTime timestamp,
+
+        int status,
+
+        String message,
+
+        List<String> errors
+){
 }
