4c941919ba67f0c71a032c5aed27ded17c653a05
gaurav-jkh <gaurav.vanani@vieraconsulting.com>
2026-07-20 17:01:57 +0530

    Phase 3: day 35,36,37: Kafka learning and integration

---
+++ backend/ai-service /src/test/java/com/shan/aidoc/documentservice/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan.aidoc.documentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
